5e44df4ad9108dbcf4438521893d3639cb61f4bd
Floyd Fernandes <floydfernandes20@gmail.com>
2022-03-26 11:16:25 +0530

    Structure layout of app.

---
+++ components/Layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+
+interface Props {
+  children: React.ReactNode;
+}
+const Layout = (props: Props) => {
+  return (
+    <div>
+      <Navbar />
+      {props.children}
+    </div>
+  );
+};
+
+export default Layout;
